93b3e693c84b962b41049d544d742713fda7a0fa
Henry-Harvey <hharvey@my.gcu.edu>
2021-11-05 22:04:37 -0700

    6

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 //Install express server
 const express = require("express");
 const path = require("path");
-
+const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(express.static(__dirname));
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<name-of-app>"));
+app.use(express.static(__dirname + "/dist/activity4-1"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/activity4-1/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, function () {
+  console.info("Angular Server App listening on port " + port);
+});
